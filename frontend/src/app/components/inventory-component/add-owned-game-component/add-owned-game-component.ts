526d5c696dbff915a6c52ffd1e298386019eeecb
Melissa Parker <mpar917@wgu.edu>
2025-09-24 10:07:35 -0700

    TASK B: Continues to work on spacing for Inventory Page

--- frontend/src/app/components/inventory-component/add-owned-game-component/add-owned-game-component.ts
+++ frontend/src/app/components/inventory-component/add-owned-game-component/add-owned-game-component.ts
@@ -1,23 +1,31 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {GameResponseDTO} from '../../../dtos/game-response-dto';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-add-owned-game-component',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './add-owned-game-component.html',
   styleUrl: './add-owned-game-component.css'
 })
 export class AddOwnedGameComponent {
+
+        clicked: boolean = false;
+
         @Input() games!: GameResponseDTO[];
 
         @Output() lookup = new EventEmitter<string>;
         triggerLookup(searchValue: string) {
           this.lookup.emit(searchValue);
+          this.clicked = true;
         }
 
         @Output() add = new EventEmitter<number>;
         triggerAdd(gameId: number) {
           this.add.emit(gameId)
+          this.clicked = false;
         }
 
 }

fc4a7ab46132a9cf34db71950f5a8ccdc7c1b5e3
Melissa Parker <mpar917@wgu.edu>
2025-10-03 16:49:31 -0700

    TASK B: Changes axis of Owned Game Play Frequency chart from y to x

--- frontend/src/app/components/analytics-component/owned-game-frequency-component/owned-game-frequency-component.ts
+++ frontend/src/app/components/analytics-component/owned-game-frequency-component/owned-game-frequency-component.ts
@@ -1,7 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {BaseChartDirective} from "ng2-charts";
 import {FormsModule} from "@angular/forms";
-import {GameResponseDTO} from '../../../dtos/game-response-dto';
 import {AnalyticsService} from '../../../services/analytics-service';
 import {ChartData, ChartOptions} from 'chart.js';
 import {BasicAnalyticsResponseDto} from '../../../dtos/analytic-dtos/basic-analytics-response-dto';
@@ -24,8 +23,8 @@ export class OwnedGameFrequencyComponent implements OnInit{
   @Input() ownedGames: OwnedGameResponseDTO[]  = [];
 
   selectedOwnedGame: OwnedGameResponseDTO | null = null;
-  selectedOwnedGameName: string | null = null;
   barChartColors: string[] = [];
+  chartHeight?: number;
 
   selectedView: string = "topFive";
 
@@ -58,8 +57,7 @@ export class OwnedGameFrequencyComponent implements OnInit{
   }
   //</editor-fold>
 
-
-
+  //<editor-fold desc="Build Colors">
   buildColors(responseLength: number): void {
        this.barChartColors = [];
        const baseColors =  ['#F25C54','#F27059', '#F4845F','#F79D65', '#F7B267']
@@ -86,14 +84,21 @@ export class OwnedGameFrequencyComponent implements OnInit{
        }
 
 }
+  //</editor-fold>
+
+  setChartHeight(responseLength: number) {
+    const barWidth = 10;
+    return barWidth * 210;
+
+  }
+
 
   //<editor-fold desc="Chart Data">
   chartData: ChartData<'bar'> = {
     labels: [],
     datasets: [
       { data: [],
-        label: 'Owned Game Play Frequency',
-        backgroundColor: ['#F25C54','#F27059', '#F4845F','#F79D65', '#F7B267']}
+        label: 'Owned Game Play Frequency'}
     ]
   };
   //</editor-fold>
@@ -137,12 +142,14 @@ export class OwnedGameFrequencyComponent implements OnInit{
 
 
     const params = this.buildParams();
+    this.chartHeight = this.setChartHeight(21);
 
 
     this.analyticsService.getOwnedGameFrequency(params).subscribe({
       next: (winLossResponse: BasicAnalyticsResponseDto) => {
 
         this.buildColors(winLossResponse.data.length);
+
 
         const max = Math.max(...winLossResponse.data);
         this.stepSize = (max / 10);
@@ -166,7 +173,7 @@ export class OwnedGameFrequencyComponent implements OnInit{
 
   //<editor-fold desc="Options">
   chartOptions: ChartOptions<'bar'> = {
-    indexAxis: 'y',
+    indexAxis: 'x',
     responsive: true,
     maintainAspectRatio: false,
     plugins: {

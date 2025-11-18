20971389704d71ab4849c378d9aadbaaa093c90b
Melissa Parker <mpar917@wgu.edu>
2025-11-17 16:10:35 -0800

    Adds env.local.js and env.prod.js to differentiate apiUrl for local and production builds

---
+++ backend/playlytics/env-config/env.prod.js
@@ -0,0 +1,3 @@
+window.__env = {
+    apiUrl: "https://playlytics.onrender.com"
+};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: (window as any).__env?.apiUrl || 'http://localhost:8080'
 };
